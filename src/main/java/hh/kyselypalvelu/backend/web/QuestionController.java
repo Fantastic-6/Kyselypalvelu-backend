283731ce249e0db91aad062847a45b839a33a14b
Supr0en <erik.ruotsalainen1@gmail.com>
2025-10-30 11:38:56 +0200

    questionRepository added to QuestionController

--- src/main/java/hh/kyselypalvelu/backend/web/QuestionController.java
+++ src/main/java/hh/kyselypalvelu/backend/web/QuestionController.java
@@ -1,33 +1,43 @@
 package hh.kyselypalvelu.backend.web;
 
+import hh.kyselypalvelu.backend.domain.QuestionRepository;
+import hh.kyselypalvelu.backend.domain.Question;
+
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class QuestionController {
+    private final QuestionRepository questionRepository;
 
-    @RequestMapping(value = "/editquestionaire")
-    public String getQuestionsList() {
-        return "editquestionaire";
+    public QuestionController(QuestionRepository questionRepository) {
+        this.questionRepository = questionRepository;
     }
 
     @RequestMapping("/savequestion")
-    public String saveQuestion() {
-        return "redirect:/editquestionaire";
+    public String saveQuestion(Question question) {
+        questionRepository.save(question);
+        return "redirect:/editquestionnaire/{id}";
     }
 
     @RequestMapping(value = "/addquestion")
-    public String addQuestion() {
+    public String addQuestion(@ModelAttribute Question question) {
+        questionRepository.save(question);
         return "addquestion";
     }
 
     @RequestMapping(value = "/deletequestion/{id}")
-    public String deleteQuestion() {
-        return "redirect:/editquestionaire";
+    public String deleteQuestion(@PathVariable("id") Long questionId) {
+        questionRepository.deleteById(questionId);
+        return "redirect:/editquestionnaire/{id}";
     }
 
     @RequestMapping(value = "/editquestion/{id}")
-    public String editQuestion() {
+    public String editQuestion(@PathVariable("id") Long questionId, Model model) {
+        model.addAttribute("question", questionRepository.findById(questionId));
         return "editquestion";
     }
 }
